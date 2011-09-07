142d5fbfd3d9783fe9e173dd51fdb0c4a1ed8930
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-09-07 09:42:21 +0700

    first commit, first release v0.0

---
+++ lib/Org/To/Pod.pm
@@ -0,0 +1,30 @@
+package Org::To::Pod;
+
+use 5.010;
+use Log::Any '$log';
+
+use vars qw($VERSION);
+
+# VERSION
+
+1;
+# ABSTRACT: Export Org document to Pod
+
+=head1 SYNOPSIS
+
+ use Org::To::Pod qw(org_to_pod);
+ my $pod = org_to_pod(source=>$org);
+
+
+=head1 DESCRIPTION
+
+NOT YET IMPLEMENTED.
+
+
+=head1 SEE ALSO
+
+L<Org::Parser>
+
+L<Org::To::HTML>
+
+=cut
