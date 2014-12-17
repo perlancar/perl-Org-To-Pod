1cef1a9f6501c1cb07f1aa5bfea68ffa91b18880
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-17 13:40:52 +0700

    [dzil] Make use of DZP::Rinci::AbstractFromMeta

--- lib/Org/To/Pod.pm
+++ lib/Org/To/Pod.pm
@@ -9,13 +9,14 @@ use Log::Any '$log';
 our %SPEC;
 $SPEC{org_to_pod} = {
     v => 1.1,
+    summary => 'Export Org document to Pod',
 };
 sub org_to_pod {
     [501, "Not yet implemented"];
 }
 
 1;
-# ABSTRACT: Export Org document to Pod
+# ABSTRACT:
 
 =head1 SYNOPSIS
 
